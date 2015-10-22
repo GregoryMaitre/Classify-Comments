7cbd215c6cf584427c6bd0b909fe82c04aa9ba93
GregoryMaitre <gregory.maitre@epfl.ch>
2015-10-22 13:05:21 +0200

    Update the questions based on my search on the irony

--- src/Model/Questions.java
+++ src/Model/Questions.java
@@ -9,14 +9,22 @@ public class Questions {
 			"Est-ce qu'il y a de l'ironie?", 
 			"Est-ce qu'il y a un smiley?", 
 			"Est-ce qu'il y a des gros mots?",
-			"Est-ce Pro ou Con la controverse?"};
+			"Est-ce Pro ou Con la controverse?",
+			"Est-ce qu'il y a une ponctuation anormale (!!!, ???, ?!, ...) ?",
+			"Est-ce qu'il y a des guillemets?",
+			"Est-ce qu'il y a des interjections?",
+			"Est-ce qu'il y a des laugher expressions?"};
 	public static String[][] ANSWERS = { 
 			{ "Commentaire positif", "Commentaire Neutre", "Commentaire negatif" },
 			{ "Commentaire long", "Commentaire court" }, 
 			{ "Présence de négation", "Absence de négation" },
 			{ "I, we", "you, he, she, they", "Aucun pronom" },
-			{ "Il y a de l'ironie", "Il n'y a pas d'ironie" },
+			{ "Il y a de l'ironie", "Il n'y a pas d'ironie", "Il y a peut-être de l'ironie" },
 			{ "Il y a un smiley positif", "Il y a un smiley négatif", "Il n'y a pas de smiley" },
 			{ "Il y a des gros mots", "Il n'y a pas de gros mots" }, 
-			{ "Pro", "Con" , "Neutre"}};
+			{ "Pro", "Con" , "Neutre"},
+			{ "Il y a une ponctuation anormale", "Il n'y a pas de ponctuation anormale"} ,
+			{ "Il y a des guillemets", "Il n'y a pas de guillemets"},
+			{ "Il y a des interjections", "Il n'y a pas d'interjections"},
+			{ "Il y a des laugher expressions", "Il n'y a pas de laugher expressions"}};
 }
